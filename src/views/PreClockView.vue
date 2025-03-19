28e0a08987bc9860934a177b40a70e12d2b98236
xueyixin <>
2025-03-19 14:32:11 +0800

    20250319 更新预览

--- src/views/PreClockView.vue
+++ src/views/PreClockView.vue
@@ -1,17 +1,11 @@
-/*
- * 预览插画
- * @author： 薛先生
- * @createDate: 2022-07-20
- * 2022-06-25 增加每小时定时修正计时器
- */
 <template>
   <div class="FlipClock">
-    <Flipper ref="flipperHour1" v-bind:style="{zoom:wzoom}"/>
-    <Flipper ref="flipperHour2" v-bind:style="{zoom:wzoom}"/>
-    <em v-bind:style="{zoom:wzoom}">:</em>
-    <Flipper ref="flipperMinute1" v-bind:style="{zoom:wzoom}"/>
-    <Flipper ref="flipperMinute2" v-bind:style="{zoom:wzoom}"/>
-    <p>{{bjtime.format("YYYY-MM-DD HH:mm:ss, MMMM, dddd")}}</p>
+    <Flipper ref="flipperMonth1" v-bind:style="{zoom:wzoom}"/>
+    <Flipper ref="flipperMonth2" v-bind:style="{zoom:wzoom}"/>
+    <em v-bind:style="{zoom:wzoom}">-</em>
+    <Flipper ref="flipperDay1" v-bind:style="{zoom:wzoom}"/>
+    <Flipper ref="flipperDay2" v-bind:style="{zoom:wzoom}"/>
+    <p>{{ bjtime.format("YYYY-MM-DD HH:mm:ss, MMMM, dddd") }}</p>
     <img :src="currentAdPicture" :alt="adPicture" ref="adPic"/>
   </div>
 </template>
@@ -31,47 +25,53 @@ export default {
       flipObjs: [], // 翻牌数字列表
       wzoom: Math.min(document.body.clientWidth / 320, 2.5), // 放大倍数
       bjtime: moment().utcOffset(480), // 北京时间
-      adPicture: 1, // 广告图片
-      adNum: 1, // 当前广告号
       adPictures: [], // 广告图片列表
-      currentAdPicture: '' // 当前广告图片 URL
+      currentAdPicture: '', // 当前广告图片 URL
+      currentDateIndex: 0 // 当前日期索引
     }
   },
   components: {
     Flipper
   },
   methods: {
-    // 初始化数字
+    // 初始化
     init () {
       this.fclock()
       // 启动整分钟倒计时
       this.bootimer = setTimeout(this.run, (60 - this.bjtime.second()) * 1000 - this.bjtime.millisecond())
-      this.adtimer = setTimeout(this.ad, 3000) // 更换未来图片
+      this.adtimer = setTimeout(this.nextDateAndPicture, 3000) // 3秒后切换到下一天
     },
     // 开始计时
     run () {
       this.fclock()
       this.bootimer = setTimeout(this.run, (60 - this.bjtime.second()) * 1000 - this.bjtime.millisecond())
     },
-    // 广告更换
-    ad () {
-      // 更换图片，不加空白
-      if (this.adNum >= this.adPictures.length) {
-        this.adNum = 0
-      }
-      this.currentAdPicture = this.adPictures[this.adNum]
-      this.adNum += 1
-      this.adtimer = setTimeout(this.ad, 3000) // 更换未来图片
+    // 切换到下一天
+    nextDateAndPicture () {
+      // 更新日期
+      const nextDate = moment(this.bjtime).add(1, 'days') // 增加一天
+      this.bjtime = nextDate
 
-      // 兼容显示
-      objectFitImages(this.$refs.adPic)
+      // 更新日期索引
+      this.currentDateIndex += 1
+      if (this.currentDateIndex >= this.adPictures.length) {
+        this.currentDateIndex = 0 // 如果超出范围，回到第一天
+      }
+
+      // 更新图片
+      this.currentAdPicture = this.adPictures[this.currentDateIndex]
+
+      // 更新翻牌显示
+      this.fclock()
+
+      // 3秒后继续切换
+      this.adtimer = setTimeout(this.nextDateAndPicture, 3000)
     },
-    // 显示时钟
+    // 显示日期
     fclock () {
-      this.bjtime = moment().utcOffset(480)
-      const nextTimeStr = this.bjtime.format('HHmmss')
+      const nextDateStr = this.bjtime.format('MMDD') // 格式化为 MMDD
       for (let i = 0; i < this.flipObjs.length; i++) {
-        this.flipObjs[i].setFront(nextTimeStr[i])
+        this.flipObjs[i].setFront(nextDateStr[i])
       }
     },
     // 加载广告图片列表
@@ -79,18 +79,21 @@ export default {
       try {
         const response = await axios.get('/images.json')
         this.adPictures = response.data.map(photo => photo.photo_url)
-        this.currentAdPicture = this.adPictures[0]
+        // 从今日日期开始
+        const today = moment().utcOffset(480).date()
+        this.currentDateIndex = today - 1
+        this.currentAdPicture = this.adPictures[this.currentDateIndex]
       } catch (error) {
-        // console.error('Failed to load ad pictures:', error)
+        console.error('Failed to load ad pictures:', error)
       }
     }
   },
   mounted () {
     this.flipObjs = [
-      this.$refs.flipperHour1,
-      this.$refs.flipperHour2,
-      this.$refs.flipperMinute1,
-      this.$refs.flipperMinute2
+      this.$refs.flipperMonth1,
+      this.$refs.flipperMonth2,
+      this.$refs.flipperDay1,
+      this.$refs.flipperDay2
     ]
     objectFitImages(this.$refs.adPic)
     this.loadAdPictures().then(() => {
